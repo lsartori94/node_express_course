823ccc24a3b4c0091af553d834cecebe92be768e
Luca Sartori <luca.sartori@globant.com>
2016-05-05 15:39:20 -0300

    Added some routes

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var express = require('express');
 var app = express();
 app.use(app.router);
+
+app.get('/saludo', function(req, res) {
+    var nombre = req.query.nombre;
+    res.send('<h1>Hola ' + saludador.saludar(nombre) + '</h1>');
+});
+
+app.get('/despedida', function(req, res) {
+    var nombre = req.query.nombre;
+    res.send('<h1>Chau ' + saludador.saludar(nombre) + '</h1>');
+});
